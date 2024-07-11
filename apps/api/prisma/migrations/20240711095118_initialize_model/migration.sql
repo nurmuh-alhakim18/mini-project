9ddebf5812dd391db1cc23962779cd531672a105
nurmuh-alhakim18 <alhakim2318@gmail.com>
2024-07-11 17:31:18 +0700

    build(prisma): initialize prisma model

---
+++ apps/api/prisma/migrations/20240711095118_initialize_model/migration.sql
@@ -0,0 +1,133 @@
+-- CreateTable
+CREATE TABLE `Role` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `EventCategory` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `TransactionStatus` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `roleId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `categoryId` INTEGER NOT NULL,
+    `oraganizerId` INTEGER NOT NULL,
+    `img` VARCHAR(191) NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `isFree` BOOLEAN NOT NULL,
+    `price` DOUBLE NULL,
+    `date` DATETIME(3) NOT NULL,
+    `startTime` TIME NOT NULL,
+    `endTime` TIME NOT NULL,
+    `location` VARCHAR(191) NOT NULL,
+    `description` TEXT NOT NULL,
+    `availableSeats` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Transaction` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `eventId` INTEGER NOT NULL,
+    `transactionStatusId` INTEGER NOT NULL,
+    `quantity` INTEGER NOT NULL,
+    `totalPrice` DOUBLE NOT NULL,
+    `discountPrice` DOUBLE NOT NULL,
+    `finalPrice` DOUBLE NOT NULL,
+    `transactionDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Promotion` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `eventId` INTEGER NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `discount` DOUBLE NOT NULL,
+    `startDate` DATETIME(3) NOT NULL,
+    `endDate` DATETIME(3) NOT NULL,
+    `quantity` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Review` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `eventId` INTEGER NOT NULL,
+    `rating` INTEGER NOT NULL,
+    `comment` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_roleId_fkey` FOREIGN KEY (`roleId`) REFERENCES `Role`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Event` ADD CONSTRAINT `Event_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `EventCategory`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Event` ADD CONSTRAINT `Event_oraganizerId_fkey` FOREIGN KEY (`oraganizerId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Transaction` ADD CONSTRAINT `Transaction_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Transaction` ADD CONSTRAINT `Transaction_eventId_fkey` FOREIGN KEY (`eventId`) REFERENCES `Event`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Transaction` ADD CONSTRAINT `Transaction_transactionStatusId_fkey` FOREIGN KEY (`transactionStatusId`) REFERENCES `TransactionStatus`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Promotion` ADD CONSTRAINT `Promotion_eventId_fkey` FOREIGN KEY (`eventId`) REFERENCES `Event`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Review` ADD CONSTRAINT `Review_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Review` ADD CONSTRAINT `Review_eventId_fkey` FOREIGN KEY (`eventId`) REFERENCES `Event`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,168 @@
+import prisma from '../src/utils/db';
+
+async function main() {
+  // -- Create user roles
+  const customer = await prisma.role.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      name: 'customer',
+    },
+  });
+
+  const admin = await prisma.role.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      name: 'admin',
+    },
+  });
+
+  // -- Create event categories
+  const sport = await prisma.eventCategory.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      name: 'sport',
+    },
+  });
+
+  const music = await prisma.eventCategory.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      name: 'music',
+    },
+  });
+
+  const movie = await prisma.eventCategory.upsert({
+    where: { id: 3 },
+    update: {},
+    create: {
+      name: 'movie',
+    },
+  });
+
+  const food = await prisma.eventCategory.upsert({
+    where: { id: 4 },
+    update: {},
+    create: {
+      name: 'food',
+    },
+  });
+
+  const business = await prisma.eventCategory.upsert({
+    where: { id: 5 },
+    update: {},
+    create: {
+      name: 'business',
+    },
+  });
+
+  const travel = await prisma.eventCategory.upsert({
+    where: { id: 6 },
+    update: {},
+    create: {
+      name: 'travel',
+    },
+  });
+
+  const education = await prisma.eventCategory.upsert({
+    where: { id: 7 },
+    update: {},
+    create: {
+      name: 'education',
+    },
+  });
+
+  const technology = await prisma.eventCategory.upsert({
+    where: { id: 8 },
+    update: {},
+    create: {
+      name: 'technology',
+    },
+  });
+
+  const health = await prisma.eventCategory.upsert({
+    where: { id: 9 },
+    update: {},
+    create: {
+      name: 'health',
+    },
+  });
+
+  const fashion = await prisma.eventCategory.upsert({
+    where: { id: 10 },
+    update: {},
+    create: {
+      name: 'fashion',
+    },
+  });
+
+  const comedy = await prisma.eventCategory.upsert({
+    where: { id: 11 },
+    update: {},
+    create: {
+      name: 'comedy',
+    },
+  });
+
+  const gaming = await prisma.eventCategory.upsert({
+    where: { id: 12 },
+    update: {},
+    create: {
+      name: 'gaming',
+    },
+  });
+
+  const art = await prisma.eventCategory.upsert({
+    where: { id: 13 },
+    update: {},
+    create: {
+      name: 'art',
+    },
+  });
+
+  const other = await prisma.eventCategory.upsert({
+    where: { id: 14 },
+    update: {},
+    create: {
+      name: 'other',
+    },
+  });
+
+  // -- Create transaction statuses
+  const pending = await prisma.transactionStatus.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      name: 'pending',
+    },
+  });
+
+  const success = await prisma.transactionStatus.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      name: 'success',
+    },
+  });
+
+  const failed = await prisma.transactionStatus.upsert({
+    where: { id: 3 },
+    update: {},
+    create: {
+      name: 'failed',
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
